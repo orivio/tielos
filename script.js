1ad05cd7cc6d478cd134f7a51c4936a338698214
orivio <russlanhossain@gmail.com>
2026-06-15 14:03:59 -0400

    added movable windows and close button
    dont mind the placeholder colors :3

---
+++ script.js
@@ -0,0 +1,61 @@
+function timeUpd() {
+    var timetxt = document.querySelector("#timeelement");
+    timetxt.innerHTML = new Date().toLocaleString();
+}
+setInterval(timeUpd, 1000);
+
+makeDraggable(document.getElementById("aboutme"));
+
+function makeDraggable(element){
+    var initialX = 0;
+    var initialY = 0;
+    var currentX = 0;
+    var currentY = 0;
+    if (document.getElementById(element.id + "header")){
+        document.getElementById(element.id + "header").onmousedown = startDrag;
+    }
+    else{
+        element.onmousedown = startDrag;
+    }
+
+    function startDrag(e){
+        e = e || window.event;
+        e.preventDefault();
+        initialX = e.clientX;
+        initialY = e.clientY;
+        document.onmouseup = stopDrag;
+        document.onmousemove = dragWin;
+    }
+
+    function stopDrag(){
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+
+    function dragWin(e){
+        e = e || window.event;
+        e.preventDefault();
+        currentX = initialX - e.clientX;
+        currentY = initialY - e.clientY;
+        initialY = e.clientY;
+        initialX = e.clientX;
+
+        element.style.top = (element.offsetTop - currentY) + "px";
+        element.style.left = (element.offsetLeft - currentX) + "px";
+    }
+}
+
+var aboutWin = document.querySelector("#aboutme");
+var closeButton = document.querySelector("#aboutmeclosebutton");
+
+closeButton.addEventListener("click", function(){
+    closeWindow(aboutme);
+});
+
+function closeWindow(e){
+    e.style.display = "none";
+}
+
+function openWindow(e){
+    e.style.display = "flex";
+}
